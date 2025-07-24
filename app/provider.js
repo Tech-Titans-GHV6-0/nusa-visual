eb2682d7f1ae26a4400cadc7cf35f89129ddffc2
ilham-sptr <ilham.69cand@gmail.com>
2025-07-24 21:00:44 +0700

    feat: loader

--- app/provider.js
+++ app/provider.js
@@ -1,11 +1,25 @@
-'use client';
+"use client";
 
 import { SessionProvider } from "next-auth/react";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import Loader from "./components/Loader";
 
 export function Providers({ children }) {
+  const pathname = usePathname();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [pathname]);
   return (
-    <SessionProvider>
-      {children}
-    </SessionProvider>
+    <div>
+      {loading ? <Loader /> : <SessionProvider>{children}</SessionProvider>}
+    </div>
   );
 }
