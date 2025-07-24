63034b874d6172efa3632f9ba830bf36eb76bd5a
ilham-sptr <ilham.69cand@gmail.com>
2025-07-24 18:17:35 +0700

    feat: providers next-auth

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./provider";
+import { Toaster } from "react-hot-toast";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -15,10 +17,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${fontSans.variable} antialiased`}
-      >
-        {children}
+      <body className={`${fontSans.variable} antialiased`}>
+        <Providers>
+          {children}
+          <Toaster
+            position="top-center"
+            toastOptions={{
+              style: {
+                border: "1px solid #4caf50",
+                padding: "12px",
+                color: "#333",
+              },
+              success: {
+                iconTheme: {
+                  primary: "#4caf50",
+                  secondary: "#fff",
+                },
+              },
+            }}
+          />
+        </Providers>
       </body>
     </html>
   );
