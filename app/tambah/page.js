3aaf8bd43e84311e7a4ee1965077d9eed81e12f1
ilham-sptr <ilham.69cand@gmail.com>
2025-07-25 09:31:41 +0700

    feat: add sidebar in page tambah

--- app/tambah/page.js
+++ app/tambah/page.js
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 import toast from "react-hot-toast";
+import Sidebar from "../components/Sidebar";
 
 const categories = [
   "Makanan Tradisional",
@@ -14,7 +15,7 @@ const categories = [
   "Festival Budaya",
   "Kerajinan Tangan",
   "Rumah Adat",
-  "Permainan Tradisional"
+  "Permainan Tradisional",
 ];
 
 export default function TambahPage() {
@@ -57,109 +58,116 @@ export default function TambahPage() {
       body: JSON.stringify({ ...form, image: imageUrl }),
     });
 
+    
     if (res.ok) {
       toast.success("Budaya berhasil ditambahkan!");
-      setTimeout(() => router.push("/"), 2000);
       setForm({ title: "", origin: "", category: "", description: "" });
       setImageFile(null);
+
+      setTimeout(() => {
+        router.push("/"); 
+      }, 2000);
     } else {
-      alert("Gagal menambahkan budaya.");
+      toast.error("Gagal menambahkan budaya.");
     }
   };
 
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 30 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.6 }}
-      className="max-w-xl mx-auto p-6 bg-white rounded-xl shadow-md mt-8"
-    >
-      <motion.h2
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 0.3 }}
-        className="text-2xl font-bold text-[#433D3D] mb-6 text-center"
+    <div className="flex h-screen my-16 md:my-0 bg-[#B49C78]">
+      <Sidebar />
+      <motion.div
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+        className="max-w-xl mx-auto p-6 bg-white rounded-xl shadow-md mt-8 max-h-[90vh] overflow-y-auto"
       >
-        Tambah Budaya Nusantara
-      </motion.h2>
-
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <motion.input
-          whileFocus={{ scale: 1.02 }}
-          type="text"
-          name="title"
-          placeholder="Judul Budaya"
-          value={form.title}
-          onChange={handleChange}
-          className="w-full border rounded p-2 focus:outline-[#433D3D] text-gray-700"
-          required
-        />
-
-        <motion.input
-          whileFocus={{ scale: 1.02 }}
-          type="text"
-          name="origin"
-          placeholder="Asal Daerah"
-          value={form.origin}
-          onChange={handleChange}
-          className="w-full border rounded p-2 focus:outline-[#433D3D] text-gray-700"
-          required
-        />
-
-        <motion.select
-          whileFocus={{ scale: 1.02 }}
-          name="category"
-          value={form.category}
-          onChange={handleChange}
-          className="w-full border rounded text-gray-700 p-2"
-          required
+        <motion.h2
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.3 }}
+          className="text-2xl font-bold text-[#433D3D] mb-6 text-center"
         >
-          <option value="">Pilih Kategori Budaya</option>
-          {categories.map((cat, i) => (
-            <option key={i} value={cat}>
-              {cat}
-            </option>
-          ))}
-        </motion.select>
+          Tambah Budaya Nusantara
+        </motion.h2>
 
-        <motion.textarea
-          whileFocus={{ scale: 1.02 }}
-          name="description"
-          placeholder="Deskripsi budaya..."
-          value={form.description}
-          onChange={handleChange}
-          rows={4}
-          className="w-full border rounded p-2 focus:outline-[#433D3D] text-gray-700"
-          required
-        />
-
-        <motion.input
-          whileFocus={{ scale: 1.02 }}
-          type="file"
-          name="image"
-          onChange={handleImage}
-          accept="image/*"
-          className="w-full border rounded p-2 text-gray-700"
-        />
-
-        {imageFile && (
-          <motion.img
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            src={URL.createObjectURL(imageFile)}
-            alt="Preview"
-            className="w-full h-48 object-cover rounded shadow-sm"
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <motion.input
+            whileFocus={{ scale: 1.02 }}
+            type="text"
+            name="title"
+            placeholder="Judul Budaya"
+            value={form.title}
+            onChange={handleChange}
+            className="w-full border rounded p-2 focus:outline-[#433D3D] text-gray-700"
+            required
           />
-        )}
 
-        <motion.button
-          whileTap={{ scale: 0.95 }}
-          type="submit"
-          className="bg-[#433D3D] w-full text-white px-4 py-2 rounded hover:bg-[#433D3D] transition"
-        >
-          Simpan Budaya
-        </motion.button>
-      </form>
-    </motion.div>
+          <motion.input
+            whileFocus={{ scale: 1.02 }}
+            type="text"
+            name="origin"
+            placeholder="Asal Daerah"
+            value={form.origin}
+            onChange={handleChange}
+            className="w-full border rounded p-2 focus:outline-[#433D3D] text-gray-700"
+            required
+          />
+
+          <motion.select
+            whileFocus={{ scale: 1.02 }}
+            name="category"
+            value={form.category}
+            onChange={handleChange}
+            className="w-full border rounded text-gray-700 p-2"
+            required
+          >
+            <option value="">Pilih Kategori Budaya</option>
+            {categories.map((cat, i) => (
+              <option key={i} value={cat}>
+                {cat}
+              </option>
+            ))}
+          </motion.select>
+
+          <motion.textarea
+            whileFocus={{ scale: 1.02 }}
+            name="description"
+            placeholder="Deskripsi budaya..."
+            value={form.description}
+            onChange={handleChange}
+            rows={4}
+            className="w-full border rounded p-2 focus:outline-[#433D3D] text-gray-700"
+            required
+          />
+
+          <motion.input
+            whileFocus={{ scale: 1.02 }}
+            type="file"
+            name="image"
+            onChange={handleImage}
+            accept="image/*"
+            className="w-full border rounded p-2 text-gray-700"
+          />
+
+          {imageFile && (
+            <motion.img
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              src={URL.createObjectURL(imageFile)}
+              alt="Preview"
+              className="w-full h-48 object-cover rounded shadow-sm"
+            />
+          )}
+
+          <motion.button
+            whileTap={{ scale: 0.95 }}
+            type="submit"
+            className="bg-[#433D3D] w-full text-white px-4 py-2 rounded hover:bg-[#433D3D] transition"
+          >
+            Simpan Budaya
+          </motion.button>
+        </form>
+      </motion.div>
+    </div>
   );
 }
